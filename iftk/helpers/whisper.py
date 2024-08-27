4a793ecf21820688d9ad2f79f99eb07348af3f2d
Sebastian Ponce <poncegramming@gmail.com>
2024-08-27 02:44:11 -0600

    Added whisper AsyncIterator helper

---
+++ iftk/helpers/whisper.py
@@ -0,0 +1,36 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+from typing import AsyncIterator
+from asyncio import to_thread
+
+import numpy as np
+import whisper
+
+
+async def whisper_stream(
+    audio_feed: AsyncIterator[bytes], model_size: str = "tiny", language: str = "en"
+) -> AsyncIterator[str]:
+    """A stream-friendly implementation of Whisper for ASR transcript generation.
+
+    Args:
+        audio_feed (AsyncIterator[bytes]): Audio bytes AsyncIterator to generate the transcription on. Note that the chunk size
+                                           should be big enough to generate streaming transcriptions.
+        model_size (str, optional): The model size tag. Defaults to "tiny".
+        language (str, optional): The model language to generate the transcription on. Defaults to "en".
+
+    Yields:
+        AsyncIterator[str]: The text result of the transcription.
+    """
+    model = whisper.load_model(model_size)
+    async for chunk in audio_feed:
+        buffer = (
+            np.frombuffer(buffer=chunk, dtype=np.int16).flatten().astype(np.float32)
+            / 32768.0
+        )
+        if not buffer.any():
+            break
+        result = await to_thread(model.transcribe, buffer, language=language)
+        yield result["text"]
