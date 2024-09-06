97601915fec76c16d03f9dd8fb65b7565a871e8b
Sebastian Ponce <poncegramming@gmail.com>
2024-09-06 00:08:17 -0600

    Groq system-channel subclass interface

--- iftk/helpers/groq.py
+++ iftk/helpers/groq.py
@@ -3,9 +3,12 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-from typing import AsyncIterator
+from typing import AsyncIterator, Callable
 
 import groq
+
+from iftk.channel import AsyncChannel
+from iftk.system import System
 
 
 async def groq_sentence_stream(llm_stream: groq._client.AsyncStream) -> AsyncIterator:
@@ -29,3 +32,28 @@ async def groq_sentence_stream(llm_stream: groq._client.AsyncStream) -> AsyncIte
             if sentence:
                 yield sentence
             sentence = ""
+
+
+class GroqChannel(AsyncChannel):
+    def __init__(
+        self,
+        sentence_stream: groq._client.AsyncStream,
+        notify_readable: Callable[[None], None] = None,
+    ) -> None:
+        super().__init__(notify_readable)
+        self.sentence_stream = sentence_stream
+        self.groq_stream = groq_sentence_stream(sentence_stream=sentence_stream)
+
+    async def read(self) -> AsyncIterator[bytes]:
+        yield await anext(self.groq_stream)
+
+
+class ElevenlabsSystem(System):
+    async def create_async_channel(
+        self,
+        sentence_stream: groq._client.AsyncStream,
+        notify_readable: Callable[[None], None] = None,
+        **kwargs,
+    ) -> AsyncChannel:
+        groq_channel = GroqChannel(sentence_stream=sentence_stream)
+        return groq_channel
