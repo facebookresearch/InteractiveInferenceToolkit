4a759f9f86c6750a8b1d6fe5cd13ef14c9c11b81
Sebastian Ponce <poncegramming@gmail.com>
2024-09-05 23:56:54 -0600

    Add system and channel subclass interfaces for Deepgram and ElevenLabs

--- iftk/helpers/elevenlabs.py
+++ iftk/helpers/elevenlabs.py
@@ -3,9 +3,12 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-from typing import AsyncIterator, Iterator, Optional
+from typing import Any, AsyncIterator, Callable, Iterator, Optional
 
 from elevenlabs.client import ElevenLabs
+
+from iftk.channel import AsyncChannel
+from iftk.system import System
 
 
 async def eleven_stream(
@@ -21,7 +24,7 @@ async def eleven_stream(
         messages (dict, optional): The message history of the dialogue system.
 
     Yields:
-        Iterator[AsyncIterator]: A bytes iterator for playing audio using 11Labs stream audio playing function.
+        Iterator[bytes]: A bytes iterator for playing audio using 11Labs stream audio playing function.
     """
     async for sentence in sentences:
         if sentence:
@@ -29,3 +32,43 @@ async def eleven_stream(
                 text=sentence, stream=True, voice=voice
             )
             yield audio_stream
+
+
+class ElevenlabsChannel(AsyncChannel):
+    def __init__(
+        self,
+        sentence_stream: AsyncChannel,
+        eleven_client: ElevenLabs,
+        voice: Optional[str] = "Jessica",
+        notify_readable: Callable[[None], None] = None,
+    ) -> None:
+        super().__init__(notify_readable)
+        self.sentence_stream = sentence_stream
+        self.eleven_stream = eleven_stream(
+            sentences=self.write(), eleven_client=eleven_client, voice=voice
+        )
+
+    async def read(self) -> AsyncIterator[bytes]:
+        yield await anext(self.eleven_stream)
+
+    async def write(self) -> AsyncIterator[str]:
+        async for sentence in self.sentence_stream:
+            yield sentence
+
+
+class ElevenlabsSystem(System):
+    async def create_async_channel(
+        self,
+        sentence_stream: AsyncChannel,
+        eleven_client: ElevenLabs,
+        notify_readable: Callable[[None], None] = None,
+        voice: Optional[str] = "Jessica",
+        **kwargs,
+    ) -> AsyncChannel:
+        eleven_channel = ElevenlabsChannel(
+            sentence_stream=sentence_stream,
+            eleven_client=eleven_client,
+            voice=voice,
+            notify_readable=notify_readable,
+        )
+        return eleven_channel
